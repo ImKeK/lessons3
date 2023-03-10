403b9145a5a16c04062521dc05c575668d9d1671
*Egor <egorkiper21@gmail.com>
2023-03-10 19:15:56 +0300

    Question

---
+++ example3/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {12, 27, 35, 4, 41, 51, 66, 74, 85};
+
+int n = array.Length;
+int find = 12;
+
+int index = 0;
+
+while (index < n)
+{
+
+  if(array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+  index++;
+}
